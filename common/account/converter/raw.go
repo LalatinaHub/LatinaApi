64f28f0e26b289a7f1fcae18f2c3f73ce527fdb1
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-06-16 23:15:15 +0700

    Fix: Trojan WS NTLS

--- common/account/converter/raw.go
+++ common/account/converter/raw.go
@@ -67,6 +67,10 @@ func ToRaw(accounts []db.DBScheme, args ...string) string {
 				u.User = url.User(account.Password)
 			}
 
+			if tls == "" {
+				tls = "none"
+			}
+
 			// Set queries
 			q := u.Query()
 			q.Set("security", tls)
