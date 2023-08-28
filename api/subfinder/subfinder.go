952fd28a0881bfbb76ed017bffc890cd914f7349
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-08-29 06:20:09 +0700

    Better subfinder response

--- api/subfinder/subfinder.go
+++ api/subfinder/subfinder.go
@@ -6,14 +6,20 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/projectdiscovery/subfinder/v2/pkg/runner"
 )
 
+type ScanResult struct {
+	Domains []string `json:"domains"`
+}
+
 func SubfinderHandler(c *gin.Context) {
 	var (
 		domain = c.Query("domain")
+		result = ScanResult{}
 	)
 
 	subfinderOpts := &runner.Options{
@@ -24,15 +30,30 @@ func SubfinderHandler(c *gin.Context) {
 
 	subfinder, err := runner.NewRunner(subfinderOpts)
 	if err != nil {
-		c.String(http.StatusServiceUnavailable, fmt.Sprintf("Failed to create subfinder runner: %v", err))
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error":   true,
+			"message": fmt.Sprintf("Failed to create subfinder runner: %v", err),
+		})
 		return
 	}
 
 	output := &bytes.Buffer{}
 	if err = subfinder.EnumerateSingleDomainWithCtx(context.Background(), domain, []io.Writer{output}); err != nil {
-		c.String(http.StatusServiceUnavailable, fmt.Sprintf("Failed to enumerate domain: %v", err))
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error":   true,
+			"message": fmt.Sprintf("Failed to enumerate domain: %v", err),
+		})
 		return
 	}
 
-	c.String(http.StatusOK, output.String())
+	for _, sub := range strings.Split(output.String(), "\n") {
+		if sub != "" {
+			result.Domains = append(result.Domains, sub)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"error":  false,
+		"result": result,
+	})
 }
