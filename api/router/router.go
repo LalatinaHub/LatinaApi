527ab97d8ed0b73d13e4a4dbef57eeab15344542
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-11-29 18:21:21 +0700

    Reverse proxy to github pages

--- api/router/router.go
+++ api/router/router.go
@@ -3,6 +3,8 @@ package router
 import (
 	"fmt"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 
 	getRoute "github.com/LalatinaHub/LatinaApi/api/get"
@@ -11,7 +13,6 @@ import (
 	ocrRoute "github.com/LalatinaHub/LatinaApi/api/ocr"
 	parseRoute "github.com/LalatinaHub/LatinaApi/api/parse"
 	subfinderRoute "github.com/LalatinaHub/LatinaApi/api/subfinder"
-	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,25 +25,43 @@ var (
 func Start() {
 	Router.SetTrustedProxies(nil)
 
-	Router.NoRoute(func(c *gin.Context) {
-		html, _ := os.ReadFile(fmt.Sprintf("%s404.html", Public))
-
-		c.Writer.Header().Set("Content-Type", "text/html")
-		c.String(http.StatusNotFound, string(html))
-	})
-
-	Router.Use(static.Serve("/", static.LocalFile(Public, false)))
+	// Penggunaan middleware RateLimiter
 	Router.Use(middleware.RateLimiter())
 
+	// Route yang digunakan pada aplikasi
 	Router.GET("/get", getRoute.GetHandler)
 	Router.GET("/log", logRoute.LogHandler)
 	Router.POST("/parse", parseRoute.ParseHandler)
 	Router.GET("/subfinder", subfinderRoute.SubfinderHandler)
 	Router.GET("/ocr", ocrRoute.OcrHandler)
 
+	// Middleware untuk reverse proxy
+	Router.NoRoute(ReverseProxy)
+
+	// Menetapkan port untuk server
 	if Port == "" {
 		Port = "8080"
 	}
 	fmt.Println("Server listening on port " + Port)
 	Router.Run(":" + Port)
+}
+
+// Middleware ReverseProxy untuk meneruskan permintaan ke backend
+func ReverseProxy(c *gin.Context) {
+	remote, err := url.Parse("http://lalatinahub.github.io/")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header // Opsional, tergantung kebutuhan
+		req.Host = remote.Host
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+		req.URL.Path = fmt.Sprintf("/LatinaDocs%s", c.Request.URL.Path)
+	}
+
+	proxy.ServeHTTP(c.Writer, c.Request)
 }
