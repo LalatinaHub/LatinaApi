2462bf6f1d7dbbc6debe3e6db53a8413c42c502a
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2024-06-06 05:10:31 +0700

    Update premium.go

--- common/member/premium.go
+++ common/member/premium.go
@@ -71,7 +71,7 @@ func GenerateDBSchemes(accountData PremiumData, c *gin.Context) []db.DBScheme {
 		vpsInfo geoip.GeoIpJson
 	)
 
-	resp, err := apiHelper.Fetch("https://" + accountData.Domain + "/info")
+	resp, err := apiHelper.Fetch("http://" + accountData.Domain + "/info")
 	if err != nil {
 		return []db.DBScheme{}
 	}
